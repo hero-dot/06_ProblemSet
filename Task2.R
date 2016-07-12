4d8e51a1d20ec2ad1fe2cd5be7e4c1a00dc1225f
Cedric <Cedric>
2016-07-12 18:33:54 +0200

    aufgabe a

--- Task2.R
+++ Task2.R
@@ -1,8 +1,48 @@
+install.packages("tidyr")
 require(cluster)
 require(ggplot2)
-voteData <- votes.repub
-t(voteData)
+require(tidyr)
+require(dplyr)
+require(rvest)
+require(magrittr)
+
 
 # a. 
 
-sfdfd
+voteData <- votes.repub
+
+
+lapply(colnames(voteData),function(x)as.numeric(gsub("X","",x)))-> date
+data.frame(date)->date
+
+voteData <- rbind(voteData, date)
+
+voteData <- as.data.frame(t(voteData))
+
+voteData%>%
+  gather(State,Percentage, Alabama:Wyoming)-> voteDataWide
+
+colnames(voteDataWide)[1] <- "Date"
+
+voteDataWide%>%
+  filter(., !is.na(Percentage))%>%
+  ggplot(.,aes(Date,Percentage))+
+  geom_line(aes(colour = State)) -> graph2a
+
+
+graph2a
+# sehr unübersichtliche Darstellung, da zu viele Linien in einem Plot; zu viele ähnliche Farbnuancen
+
+
+#b.
+
+
+
+  url <- "http://www.cdc.gov/std/stats11/census.htm"
+  url%>%
+    read_html()%>%
+    html_nodes("table")%>%
+    html_table()%>%
+    .[[1]]-> regionTable
+
+regionTable
